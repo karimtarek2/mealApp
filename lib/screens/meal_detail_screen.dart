064c082aed2379341340947c12d9895d98db24b7
Karim <karim@Ahmed.local>
2022-12-01 12:52:56 +0200

    edit2

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:meal_app/dummy_data.dart';
 
@@ -27,7 +28,7 @@ class MealDetailScreen extends StatelessWidget {
       ),
       margin: EdgeInsets.all(10),
       padding: EdgeInsets.all(10),
-      height: 150,
+      height: 500,
       width: 300,
       child: child,
     );
@@ -37,6 +38,9 @@ class MealDetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final mealId = ModalRoute.of(context)?.settings.arguments as String;
     final selectedMeal = DUMMY_MEALS.firstWhere((meal) => meal.id == mealId);
+    List listOfIngredients = selectedMeal.ingredients;
+    List listOfSteps = selectedMeal.steps;
+
     return Scaffold(
       appBar: AppBar(
         title: Text(selectedMeal.title),
@@ -53,51 +57,117 @@ class MealDetailScreen extends StatelessWidget {
               ),
             ),
             buildSectionTitle(context, 'Ingredients'),
-            Container(
-              width: double.infinity,
-              child: buildContainer(ListView.builder(
-                itemBuilder: (ctx, index) => Card(
-                  color: Colors.amber,
-                  child: Padding(
-                    padding:
-                        const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-                    child: Text(selectedMeal.ingredients[index]),
-                  ),
-                ),
-                itemCount: selectedMeal.ingredients.length,
-              )),
-            ),
-            buildSectionTitle(context, 'Steps'),
-            Container(
-              width: double.infinity,
-              child: buildContainer(ListView.builder(
-                itemBuilder: (ctx, index) => Column(
-                  children: [
-                    ListTile(
-                      leading: CircleAvatar(
-                        backgroundColor: Colors.pink,
-                        child: Text('# ${index + 1}'),
+
+            Column(
+              children: [
+                ...listOfIngredients.map((e) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 5),
+                    child: Container(
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: Colors.amber,
+                        borderRadius: BorderRadius.circular(10),
                       ),
-                      title: Text(
-                        selectedMeal.steps[index],
-                        style: TextStyle(fontSize: 15),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(e),
                       ),
                     ),
-                    Divider(),
-                  ],
-                ),
-                itemCount: selectedMeal.steps.length,
-              )),
+                  );
+                })
+              ],
             ),
+
+            // Container(
+            //   width: double.infinity,
+            //   child: buildContainer(
+            //     ListView.builder(
+            //       itemBuilder: (ctx, index) => Card(
+            //         color: Colors.amber,
+            //         child: Padding(
+            //           padding: const EdgeInsets.symmetric(
+            //               vertical: 5, horizontal: 10),
+            //           child: Text(selectedMeal.ingredients[index]),
+            //         ),
+            //       ),
+            //       itemCount: selectedMeal.ingredients.length,
+            //     ),
+            //   ),
+            // ),
+            buildSectionTitle(context, 'Steps'),
+            Column(
+              children: [
+                ...listOfSteps.map((z) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 5),
+                    child: Container(
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: Colors.amber.shade700,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                            children: [
+                                  ListTile(
+                                    leading: CircleAvatar(
+                                       backgroundColor: Colors.pink,
+                                       child: Text('#'),
+                                     ),
+                                     title: Text(
+                                       z,
+                                       style: TextStyle(fontSize: 15),
+                                     ),
+                                   ),
+                                   Divider(),
+                                 ],
+                        ),
+                      ),
+                    ),
+                  );
+                })
+              ],
+            ),
+            // Container(
+            //   width: double.infinity,
+            //   child: buildContainer(ListView.builder(
+            //     itemBuilder: (ctx, index) => Column(
+            //       children: [
+            //         ListTile(
+            //           leading: CircleAvatar(
+            //             backgroundColor: Colors.pink,
+            //             child: Text('# ${index + 1}'),
+            //           ),
+            //           title: Text(
+            //             selectedMeal.steps[index],
+            //             style: TextStyle(fontSize: 15),
+            //           ),
+            //         ),
+            //         Divider(),
+            //       ],
+            //     ),
+            //     itemCount: selectedMeal.steps.length,
+            //   )),
+            // ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: ()=> toggleFavorite(mealId),
+        onPressed: () => toggleFavorite(mealId),
         child: Icon(
           isFavorite(mealId) ? Icons.star : Icons.star_border,
         ),
       ),
     );
+  }
+
+  Widget? cards(List<dynamic> listOfIngredients) {
+    for (int i = 0; i < listOfIngredients.length; i++) {
+      return Card(
+        child: Text(listOfIngredients[i].toString()),
+      );
+    }
   }
 }
